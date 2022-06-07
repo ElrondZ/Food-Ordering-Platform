1ee5fd64bcd9482a0cd3e7f1a03aa05fe44b8a90
lewis <zl4023@nyu.edu>
2022-06-07 10:18:02 -0400

    Test

---
+++ src/test/java/com/omtou/ruiji/RuijiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omtou.ruiji;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RuijiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
