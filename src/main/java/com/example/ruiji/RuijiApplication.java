baf24ee82379aea35192a021928f572d79557b0a
Yan <745204244@qq.com>
2022-06-07 10:59:03 -0400

    2

--- src/main/java/com/example/ruiji/RuijiApplication.java
+++ src/main/java/com/example/ruiji/RuijiApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RuijiApplication {
-
+//hello
     public static void main(String[] args) {
         SpringApplication.run(RuijiApplication.class, args);
     }
