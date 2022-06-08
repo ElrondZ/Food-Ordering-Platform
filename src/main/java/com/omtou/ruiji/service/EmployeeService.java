a41751ac761d7798af1f224aa7f5973cab01a72a
lewis <zl4023@nyu.edu>
2022-06-08 11:10:28 -0400

    Project Day1

---
+++ src/main/java/com/omtou/ruiji/service/EmployeeService.java
@@ -0,0 +1,11 @@
+package com.omtou.ruiji.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.omtou.ruiji.entity.Employee;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface EmployeeService extends IService<Employee> {
+
+
+}
---
+++ src/main/java/com/omtou/ruiji/service/impl/EmployeeServiceImpl.java
@@ -0,0 +1,13 @@
+package com.omtou.ruiji.service.impl;
+
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.omtou.ruiji.entity.Employee;
+import com.omtou.ruiji.mapper.EmployeeMapper;
+import com.omtou.ruiji.service.EmployeeService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmployeeServiceImpl extends ServiceImpl<EmployeeMapper, Employee> implements EmployeeService {
+
+}
