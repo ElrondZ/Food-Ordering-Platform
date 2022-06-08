1f9faaa5fbc357bda9a64b9d997a0bccf40abf1d
Yan <745204244@qq.com>
2022-06-08 17:44:14 -0400

    day2

--- src/main/java/com/example/ruiji/RuijiApplication.java
+++ src/main/java/com/example/ruiji/RuijiApplication.java
@@ -3,9 +3,11 @@ package com.example.ruiji;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
 @Slf4j
+@ServletComponentScan
 public class RuijiApplication {
 //hello
     public static void main(String[] args) {
--- src/main/java/com/example/ruiji/controller/EmployeeController.java
+++ src/main/java/com/example/ruiji/controller/EmployeeController.java
@@ -1,19 +1,19 @@
 package com.example.ruiji.controller;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.example.ruiji.common.R;
 import com.example.ruiji.entity.Employee;
 import com.example.ruiji.mapper.EmployeeMapper;
 import com.example.ruiji.service.EmployeeService;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.DigestUtils;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDateTime;
 
 @Slf4j
 @RestController
@@ -51,5 +51,60 @@ public class EmployeeController {
     public R<String> logout(HttpServletRequest request){
         request.getSession().removeAttribute("employee");
         return R.success("Logout!");
+    }
+
+    @PostMapping
+    public R<String> save(HttpServletRequest request, @RequestBody Employee employee){
+        log.info("新增员工，员工信息{}", employee.toString() );
+
+        employee.setPassword(DigestUtils.md5DigestAsHex("123456".getBytes()));
+
+        employee.setCreateTime(LocalDateTime.now());
+        employee.setUpdateTime(LocalDateTime.now());
+
+        long empID = (long) request.getSession().getAttribute("employee");
+
+        employee.setCreateUser(empID);
+        employee.setUpdateUser(empID);
+
+        employeeService.save(employee);
+
+        return R.success("成功！");
+    }
+
+    @GetMapping("/page")
+    public R<Page> page(int page, int pageSize, String name){
+        log.info("page = {}, pageSize = {}, name = {}", page, pageSize, name);
+
+        Page pageInfo = new Page(page, pageSize);
+
+        LambdaQueryWrapper<Employee> queryWrapper = new LambdaQueryWrapper();
+
+        queryWrapper.like(StringUtils.isNotEmpty(name), Employee::getName, name);
+
+        queryWrapper.orderByDesc(Employee::getUpdateTime);
+
+        employeeService.page(pageInfo, queryWrapper);
+
+        return R.success(pageInfo);
+    }
+    @PutMapping
+    public R<String> update(HttpServletRequest request, @RequestBody Employee employee){
+        log.info(employee.toString());
+
+        long empId = (long)request.getSession().getAttribute("employee");
+        employee.setUpdateTime(LocalDateTime.now());
+        employee.setUpdateUser(empId);
+        employeeService.updateById(employee);
+
+        return R.success("信息修改成功！");
+    }
+
+    @GetMapping("/{id}")
+    public R<Employee> getById(@PathVariable long id){
+        log.info("根据ID查询..");
+        Employee employee = employeeService.getById(id);
+        if(employee != null) return R.success(employee);
+        return R.error("没有对应信息...");
     }
 }
